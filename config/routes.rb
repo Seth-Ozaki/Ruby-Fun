fccf3c566c6c0521e74f4e69a86e1b15c3f4594d
Seth-Ozaki <sethozaki123@gmail.com>
2024-09-16 17:50:41 -0600

    Added some Crud operation

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  # Render dynamic PWA files from app/views/pwa/*
-  get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-  get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "blog_posts#index"
+
+  get "blog_posts/new" => "blog_posts#new", as: :new_blog
+  post "blog_posts" => "blog_posts#create", as: :blog_posts
+
+  # dynamic routes go last
+  get "blog_posts/:id" => "blog_posts#show", as: :blog
+  get "blog_posts/:id/edit" => "blog_posts#edit", as: :edit_blog
+
 end
