6f47749e04c702dee1b83dd23dfd13c5efeab1aa
Seth-Ozaki <sethozaki123@gmail.com>
2024-09-17 12:09:00 -0600

    finished crud ops

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -12,6 +12,7 @@ class BlogPostsController < ApplicationController
   def new
     @blog_post = BlogPost.new
   end
+  
 
   def create
     @blog_post = BlogPost.new(blog_post_params)
@@ -24,6 +25,20 @@ class BlogPostsController < ApplicationController
 
   def edit 
     @blog_post = BlogPost.find(params[:id])
+  end
+
+  def update
+    @blog_post = BlogPost.find(params[:id])
+    if @blog_post.update(blog_post_params)
+      redirect_to blog_path(@blog_post)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def delete
+    @blog_post = BlogPost.destroy(params[:id])
+    redirect_to root_path
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,20 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "blog_posts#index"
-
+  
   get "blog_posts/new" => "blog_posts#new", as: :new_blog
-  post "blog_posts" => "blog_posts#create", as: :blog_posts
-
+  post "blog_posts/new" => "blog_posts#create", as: :all_blogs
+  
   # dynamic routes go last
   get "blog_posts/:id" => "blog_posts#show", as: :blog
+  patch "blog_posts/:id" => "blog_posts#update"
   get "blog_posts/:id/edit" => "blog_posts#edit", as: :edit_blog
-
+  delete "blog_posts/:id" => "blog_posts#delete", as: :delete_blog
+  
+  # devise routes
+  devise_for :users
+  
+  
+  root "blog_posts#index"
+  resources :blog_posts
 end
